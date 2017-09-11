3d224869e776707b3af10d260947913534b5be0d
Anna M <anna.michalik@possible.com>
2017-09-11 15:03:50 +0200

    Console appliation with regular expresions created

---
+++ ConsoleRegexTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRegexTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "C:\\wewr\\asdasda\\tw.docx";
+            Console.WriteLine("{0} is valid {1}", str, RegularExpressionTest.IsValid(str));
+            str = "C:\\wewr\\asdasda\\tw.1234567";
+            Console.WriteLine("{0} is valid {1}", str, RegularExpressionTest.IsValid(str));
+            RegularExpressionTest.MakeTest();
+            RegularExpressionTest.DoLAzy();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleRegexTest/RegularExpressionTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleRegexTest
+{
+    public class RegularExpressionTest
+    {
+
+        public static void MakeTest()
+        {
+
+            Console.WriteLine(SeparateWords("SMBrosO"));
+            Console.WriteLine(SeparateWords("TryAgainWithMoreWorkds"));
+            Console.WriteLine(SeparateWords("OneTWOThree"));
+        }
+
+        // from blog http://adamprescott.net/
+        public static string SeparateWords(string value)
+        {
+            var x = Regex.Replace(value, @"([A-Z][^A-Z])", " $1");
+            x = Regex.Replace(x, "([a-z])([A-Z])", "$1 $2");
+            x = x.Trim();
+            return x;
+        }
+
+        /// <summary>
+        /// Test string using Regex.IsMatch static method.
+        /// </summary>
+        public static bool IsValid(string value)
+        {
+            string matchString =
+                @"^.*\\(.){1,2}\.(.){1,6}$";
+            return Regex.IsMatch(value, matchString);
+        }
+
+        public static void DoLAzy()
+        {
+
+            string msg = "=?windows-1258?B?UkU6IFRyIDogUGxhbiBkZSBjb250aW51aXTpIGQnYWN0aXZpdOkgZGVz?= =?windows-1258?B?IHNlcnZldXJzIFdlYiBHb1ZveWFnZXN=?=";
+            var charSetOccurences = new Regex(@"=\?.*?\?[BQ]\?.*?\?=", RegexOptions.IgnoreCase);
+            MatchCollection matches = charSetOccurences.Matches(msg);
+            foreach (Match match in matches)
+            {
+                string[] encoding = match.Groups[0].Value.Split(new string[] { "?" }, StringSplitOptions.None);
+                string charSet = encoding[1];
+                string encodeType = encoding[2];
+                string encodedString = encoding[3];
+                Console.WriteLine("Charset: " + charSet);
+                Console.WriteLine("Encoding type: " + encodeType);
+                Console.WriteLine("Encoded String: " + encodedString + "\n");
+            }
+
+            charSetOccurences = new Regex(@"=\?.*\?B\?.*\?=", RegexOptions.IgnoreCase);
+            var charSetMatches = charSetOccurences.Matches(msg);
+            foreach (Match match in charSetMatches)
+            {
+                Console.WriteLine("greedy {0}", match);
+                //charSet = match.Groups[0].Value.Replace("=?", "").Replace("?B?", "").Replace("?b?", "");
+            }
+
+        }
+    }
+}
