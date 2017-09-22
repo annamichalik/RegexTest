a99906d2b2b0a55d0b922b170ee057d64df3b282
Anna M <anna.michalik@possible.com>
2017-09-22 11:06:16 +0200

    replace reserved Charactersin url

---
+++ ConsoleRegexTest/UrlReplacements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleRegexTest
+{
+    public static class UrlReplacements
+    {
+
+        public static string ReplaceReservedCharacters(string value)
+        {
+            var x = Regex.Replace(value.Trim(), @"([^a-zA-Z0-9_-])", "_");
+            return x;
+        }
+    }
+}
---
+++ UnitTestRegexTest/RegexUrlTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleRegexTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace UnitTestRegexTest
+{
+    [TestClass]
+    public class RegexUrlTests
+    {
+        [Test]
+        public void TrueWithDigit([Values("9Baa!deKL", "9Baa?deKL", "9Baa deKL", "9Baa&deKL")] string value)
+        {
+            var text = UrlReplacements.ReplaceReservedCharacters(value);
+            Assert.AreEqual("9Baa_deKL",text);
+        }
+
+    }
+}
